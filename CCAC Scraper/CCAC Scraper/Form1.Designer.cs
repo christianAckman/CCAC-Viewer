8c9da078909f0b165733a237e2bfeadfc6dd1ac7
Christian Ackman <ackmanChristian@gmail.com>
2017-04-17 13:32:55 -0400

    initial

---
+++ CCAC Scraper/CCAC Scraper/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CCAC_Scraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGetSubjects = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.listBox_Subjects = new System.Windows.Forms.ListBox();
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnGetClasses = new System.Windows.Forms.Button();
+            this.btnGetSyllabus = new System.Windows.Forms.Button();
+            this.dtg_Courses = new System.Windows.Forms.DataGridView();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_Courses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetSubjects
+            // 
+            this.btnGetSubjects.Location = new System.Drawing.Point(932, 51);
+            this.btnGetSubjects.Name = "btnGetSubjects";
+            this.btnGetSubjects.Size = new System.Drawing.Size(89, 23);
+            this.btnGetSubjects.TabIndex = 0;
+            this.btnGetSubjects.Text = "Get Subjects";
+            this.btnGetSubjects.UseVisualStyleBackColor = true;
+            this.btnGetSubjects.Click += new System.EventHandler(this.getSubjects_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(904, 808);
+            this.webBrowser.TabIndex = 1;
+            // 
+            // listBox_Subjects
+            // 
+            this.listBox_Subjects.FormattingEnabled = true;
+            this.listBox_Subjects.Location = new System.Drawing.Point(932, 80);
+            this.listBox_Subjects.Name = "listBox_Subjects";
+            this.listBox_Subjects.Size = new System.Drawing.Size(215, 745);
+            this.listBox_Subjects.TabIndex = 2;
+            // 
+            // panel
+            // 
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel.Controls.Add(this.webBrowser);
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(914, 818);
+            this.panel.TabIndex = 3;
+            // 
+            // btnGetClasses
+            // 
+            this.btnGetClasses.Location = new System.Drawing.Point(1153, 51);
+            this.btnGetClasses.Name = "btnGetClasses";
+            this.btnGetClasses.Size = new System.Drawing.Size(75, 23);
+            this.btnGetClasses.TabIndex = 5;
+            this.btnGetClasses.Text = "Get Classes";
+            this.btnGetClasses.UseVisualStyleBackColor = true;
+            this.btnGetClasses.Click += new System.EventHandler(this.btnGetClasses_Click);
+            // 
+            // btnGetSyllabus
+            // 
+            this.btnGetSyllabus.Location = new System.Drawing.Point(1360, 51);
+            this.btnGetSyllabus.Name = "btnGetSyllabus";
+            this.btnGetSyllabus.Size = new System.Drawing.Size(75, 23);
+            this.btnGetSyllabus.TabIndex = 6;
+            this.btnGetSyllabus.Text = "Get Syllabus";
+            this.btnGetSyllabus.UseVisualStyleBackColor = true;
+            this.btnGetSyllabus.Click += new System.EventHandler(this.btnGetSyllabus_Click);
+            // 
+            // dtg_Courses
+            // 
+            this.dtg_Courses.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dtg_Courses.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dtg_Courses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_Courses.Location = new System.Drawing.Point(1153, 80);
+            this.dtg_Courses.MultiSelect = false;
+            this.dtg_Courses.Name = "dtg_Courses";
+            this.dtg_Courses.ReadOnly = true;
+            this.dtg_Courses.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dtg_Courses.Size = new System.Drawing.Size(492, 745);
+            this.dtg_Courses.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1657, 831);
+            this.Controls.Add(this.dtg_Courses);
+            this.Controls.Add(this.btnGetSyllabus);
+            this.Controls.Add(this.btnGetClasses);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.listBox_Subjects);
+            this.Controls.Add(this.btnGetSubjects);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CCAC Master Syllabi";
+            this.panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_Courses)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetSubjects;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.ListBox listBox_Subjects;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btnGetClasses;
+        private System.Windows.Forms.Button btnGetSyllabus;
+        private System.Windows.Forms.DataGridView dtg_Courses;
+    }
+}
+
---
+++ CCAC Scraper/CCAC Scraper/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using AngleSharp.Parser.Html;
+using AngleSharp.Dom.Html;
+using AngleSharp.Dom;
+
+namespace CCAC_Scraper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            deleteFiles();
+        }
+
+        List<string> subjectCodes = null;
+        List<string> subjectName = null;
+        List<string> courseCodes = null;
+
+        private IDictionary<string, object> getCourses(string subjectCode)
+        {
+            IDictionary<string, object> dict = new Dictionary<string, object>();
+
+            List<string> courses = new List<string>();
+            List<string> labels = new List<string>();
+
+            string coursesURL = @"https://webapps.ccac.edu/mastersyllabi/GetCourses/tabid/57/Default.aspx?subject=" + subjectCode;
+
+            string html = getHTML(coursesURL);
+
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument doc = parser.Parse(html);
+            IElement coursesTable = doc.GetElementById("dnn_ctr375_ContentPane");
+
+            IHtmlCollection<IElement> courseUrls = coursesTable.GetElementsByTagName("a");
+            IHtmlCollection<IElement> courseLabels = coursesTable.GetElementsByClassName("Normal");
+
+            
+            int i = 0;
+
+            foreach(IElement url in courseUrls)
+            {
+                // Skip the last one
+                if (i != courseUrls.Length - 1)
+                {
+                    courses.Add(url.GetAttribute("href"));
+                }
+                i++;
+            }
+
+            int urlCounter = 0;
+
+            foreach (IElement label in courseLabels)
+            {
+                labels.Add(label.TextContent);
+
+                if (labels.Count == 3)
+                {
+                    dict.Add(courses[urlCounter], labels);
+                    urlCounter++;
+                    labels = new List<string>();
+                }
+            }
+
+            return dict;
+        }
+
+        private IDictionary<string, object> getSubjects()
+        {
+
+            IDictionary<string, object> dict = new Dictionary<string, object>();
+            List<string> codes = new List<string>();
+            List<string> subjectURLS = new List<string>();
+            List<string> list;
+
+            string subjectsURL = @"https://webapps.ccac.edu/mastersyllabi/GetCourses/tabid/57/Default.aspx?division=C";
+
+            string html = getHTML(subjectsURL);
+
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument doc = parser.Parse(html);
+            IElement subjectsTable = doc.GetElementById("dnn_ctr375_GetCourse_dgSubjects");
+
+            IHtmlCollection<IElement> subjects = subjectsTable.GetElementsByClassName("CommandButton");
+
+            foreach (IElement subject in subjects)
+            {
+                codes.Add(subject.TextContent.ToString());
+                subjectURLS.Add(subject.LastElementChild.GetAttribute("href"));
+            }
+
+            // Separate the codes and courses
+            IDictionary<string, string> codesAndCourses = parseCodes(codes);
+
+            int i = 0;
+
+            foreach(KeyValuePair<string, string> kvp in codesAndCourses)
+            {
+                list = new List<string>();
+                list.Add(kvp.Key);
+                list.Add(kvp.Value);
+
+                dict.Add(subjectURLS[i], list);
+                i++;
+            }
+
+            return dict;
+        }
+
+        public IDictionary<string, string> parseCodes(List<string> codes)
+        {
+            IDictionary<string, string> codesAndCourses = new Dictionary<string, string>();
+
+            foreach (string cd in codes)
+            {
+                // Get first space
+                int space = cd.IndexOf(' ');
+
+                // Parse
+                string course = cd.Substring(space);
+
+                // Clean up
+                if(course.StartsWith(" ")){
+                    course = course.Remove(0, 1);
+                }
+
+                // Parse
+                string code = cd.Remove(cd.Length - course.Length);
+
+                // Clean up
+                code = code.Replace(" ", "");
+
+                // Add results
+                codesAndCourses.Add(code, course);
+            }
+
+            return codesAndCourses;
+        }
+
+        private string getHTML(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader read = new StreamReader(stream, Encoding.UTF8);
+            string html = read.ReadToEnd();
+            return html;
+        }
+        
+        private byte[] getFile(string url)
+        {
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse resp = req.GetResponse();
+
+            Stream stream = resp.GetResponseStream();
+            byte[] bytes = convertToBytes(stream);
+
+            return bytes;
+        }
+        private string saveFile(string filename, byte[] bytes)
+        {
+            string projectFolder = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString();
+            string filepath = Path.Combine(projectFolder, "Temporary Internet Files") + @"\" + filename;
+            File.WriteAllBytes(filepath, bytes);
+
+            return filepath;
+        }
+
+        private void deleteFiles()
+        {
+            string projectFolder = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString();
+            string folderPath = Path.Combine(projectFolder, "Temporary Internet Files");
+
+            string[] filePaths = Directory.GetFiles(folderPath);
+            
+            foreach(string filePath in filePaths)
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        private byte[] convertToBytes(Stream stream)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+
+        private void getSubjects_Click(object sender, EventArgs e)
+        {
+            IDictionary<string, object> subjects = getSubjects();
+
+            List<string> subjectURLs = new List<string>();
+            subjectCodes = new List<string>();
+            subjectName = new List<string>();
+
+            foreach (KeyValuePair<string, object> subject in subjects)
+            {
+
+                List<string> dict = (List<string>)subject.Value;
+
+                subjectURLs.Add(subject.Key);
+                subjectCodes.Add(dict[0]);
+                subjectName.Add(dict[1]);
+
+            }
+
+            listBox_Subjects.DataSource = subjectName;
+
+        }
+    
+        private void btnGetSyllabus_Click(object sender, EventArgs e)
+        {
+            if (dtg_Courses.DataSource != null)
+            {
+                int selectedClass = dtg_Courses.CurrentCell.RowIndex;
+
+                string code = courseCodes[selectedClass];
+
+                string url = @"https://webapps.ccac.edu/mastersyllabi/GetCourses/tabid/57/Default.aspx?course=" + code;
+                byte[] bytes = getFile(url);
+
+                string filename = code + ".pdf";
+
+                string filepath = saveFile(filename, bytes);
+
+                webBrowser.Navigate(filepath);
+            }
+        }
+
+        private void btnGetClasses_Click(object sender, EventArgs e)
+        {
+            if (listBox_Subjects.DataSource != null)
+            {
+                int selectedSubject = listBox_Subjects.SelectedIndex;
+
+                IDictionary<string, object> courses = getCourses(subjectCodes[selectedSubject]);
+
+                courseCodes = new List<string>();
+
+                DataTable table = new DataTable();
+                table.Columns.Add("Code");
+                table.Columns.Add("Name");
+                table.Columns.Add("Last Updated");
+
+                foreach (KeyValuePair<string, object> course in courses)
+                {
+                    List<string> dict = (List<string>)course.Value;
+                    table.Rows.Add(dict[0], dict[1], dict[2]);
+                    courseCodes.Add(dict[0]);
+                }
+
+                dtg_Courses.DataSource = table;
+            }
+        }
+    }
+}
